0cfd6fde3a7054fbf95c83659b6a530c0292fb3e
erdec-burno <erdec@mail.ru>
2021-10-17 22:19:42 +0300

    add var to translate message

---
+++ src/components/MyLocale.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { withTranslation, WithTranslation } from "react-i18next";
+import SubLocale from './SubLocale';
+
+const MyLocale: React.FC<WithTranslation> = (props) => {
+  const { t, i18n } = props;
+
+  const changeLanguage = (language: string) => {
+    i18n.changeLanguage(language);
+  };
+  return (
+    <div className="App">
+        <button onClick={() => changeLanguage("en")}>EN</button>
+        <button onClick={() => changeLanguage("ru")}>RU</button>
+        <hr />
+        <p>====== {t("description.subtitle3")} ======</p>
+        <SubLocale/>
+    </div>
+  );
+}
+
+export default withTranslation()(MyLocale);
