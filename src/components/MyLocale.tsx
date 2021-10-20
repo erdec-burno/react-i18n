d0948f3b928c743f778c19ceda8fb8cdc36e052d
erdec-burno <erdec@mail.ru>
2021-10-20 22:40:51 +0300

    add useTranslate hook

--- src/components/MyLocale.tsx
+++ src/components/MyLocale.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { withTranslation, WithTranslation } from "react-i18next";
-import SubLocale from './SubLocale';
+import { useTranslation } from "react-i18next";
+import i18n from '../i18n';
 
-const MyLocale: React.FC<WithTranslation> = (props) => {
-  const { t, i18n } = props;
+const MyLocale: React.FC = (props) => {
+  const { t } = useTranslation();
 
   const changeLanguage = (language: string) => {
     i18n.changeLanguage(language);
   };
+  const name = "MyLocale!";
   return (
     <div className="App">
         <button onClick={() => changeLanguage("en")}>EN</button>
         <button onClick={() => changeLanguage("ru")}>RU</button>
         <hr />
-        <p>====== {t("description.subtitle3")} ======</p>
-        <SubLocale/>
+        <p>====== {t("description.subtitle3", {name})} ======</p>
+        <p>result from api backend: {t("back:description.subtitle1")}</p>
     </div>
   );
 }
 
-export default withTranslation()(MyLocale);
+export default MyLocale;
--- src/i18n.js
+++ src/i18n.js
@@ -1,28 +1,40 @@
 import i18n from 'i18next'
 import Backend from 'i18next-http-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
+// import LanguageDetector from 'i18next-browser-languagedetector'
 
 i18n
   // Подключение бэкенда i18next
   .use(Backend)
   // Автоматическое определение языка 
-  .use(LanguageDetector)
+  // .use(LanguageDetector)
   // модуль инициализации
   .use (initReactI18next)
   .init({
     lng: document.querySelector('html').lang,
     // Стандартный язык
     fallbackLng: 'ru',
-    debug: true,
+    debug: false,
     // Распознавание и кэширование языковых кук
     detection: {
-      order: ['cookie', 'queryString', 'localStorage', 'cookie'],
+      order: ['queryString', 'cookie', 'localStorage'],
       cache: ['cookie']
     },
     interpolation: {
       escapeValue: false
-    }
+    },
+    // backend: {
+    //   loadPath: '/locales2/{{lng}}/{{ns}}.json'
+    // }
   })
+
+  fetch('http://localhost:3000/locales/ru/translation.json')
+	.then((response) => {
+		return response.json();
+	})
+	.then((data) => {
+		i18n.addResourceBundle('ru', "back", data)
+	}
+);
 
 export default i18n;
